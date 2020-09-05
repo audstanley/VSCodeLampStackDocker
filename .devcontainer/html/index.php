c552077fbff38c3380904f7d8785446115adab11
Richard Stanley <audstanley@gmail.com>
2020-09-05 15:03:16 -0700

    Full Lamp stack with phpmyadmin, ready for VSCode

---
+++ .devcontainer/html/index.php
@@ -0,0 +1,11 @@
+Hello world
+<?php
+
+// Show all information, defaults to INFO_ALL
+phpinfo();
+
+// Show just the module information.
+// phpinfo(8) yields identical results.
+phpinfo(INFO_MODULES);
+
+?>
