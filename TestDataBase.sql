59998d6b2275bbcdfb5b3f2d77fcaaa415728207
root <audstanley@gmail.com>
2020-09-13 21:04:13 +0000

    sql connection success

--- TestDataBase.sql
+++ TestDataBase.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mysql-server
--- Generation Time: Sep 05, 2020 at 08:54 PM
+-- Generation Time: Sep 13, 2020 at 08:59 PM
 -- Server version: 8.0.19
 -- PHP Version: 7.4.1
 
@@ -29,8 +29,38 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `test` (
-  `testName` int NOT NULL
+  `idx` bigint UNSIGNED NOT NULL,
+  `name` tinytext NOT NULL,
+  `age` tinyint UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `test`
+--
+
+INSERT INTO `test` (`idx`, `name`, `age`) VALUES
+(1, 'Alice', 22),
+(2, 'Bob', 23);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `test`
+--
+ALTER TABLE `test`
+  ADD PRIMARY KEY (`idx`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `test`
+--
+ALTER TABLE `test`
+  MODIFY `idx` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- index.php
+++ index.php
@@ -1,5 +1,31 @@
-HELLO
-
 <?php
-phpinfo();
+/* Connect to a MySQL database using driver invocation */
+$dsn = 'mysql:dbname=TestDataBase;host=mysql-server;';
+$user = 'root';
+$password = 'secret';
+
+try {
+    $dbh = new PDO($dsn, $user, $password);
+    echo 'SUCCESSFULLY CONNECTED TO MYSQL </br>';
+    $myArray = range(0, 3);
+    
+    $sql = 'SELECT * FROM test';
+    $sth = $dbh->prepare($sql);
+    $sth->execute();
+    $arr = $sth->fetchAll();
+    
+    // TestDataBase -> test -> idx:1,name:Alice,age:22
+    echo $arr[0][1] . ' is ' . $arr[0][2] . ' years old.';
+    echo '</br>';
+
+    // TestDataBase -> test -> idx:1,name:Bob,age:23
+    echo $arr[1][1] . ' is ' . $arr[1][2] . ' years old.';
+    echo '</br>';
+
+    phpinfo();
+    
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
+
 ?>
